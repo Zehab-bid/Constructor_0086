7c9b15d1aa499644e663b3963c8ac7380408c7f8
Zehab-bid <zehabidha152gmail.com>
2025-05-20 16:07:57 +0700

    end

---
+++ latihan.cpp
@@ -0,0 +1,39 @@
+//buatlah program c++ dengan class yang memiliki atribut : namaBarang,
+//kodeBarang
+//program memliki constructor berparameter
+//untuk mengisi nilai namaBarang dan kodeBarang
+//serta memiliki fungsi untuk menampilkan informasi barang
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class barang {
+    public:
+    int kodeBarang;
+    string namaBarang;
+
+    barang (int kode, string nama) {
+        kodeBarang = kode;
+        namaBarang = nama;
+    }
+
+    void output()
+    {
+        cout << "nama barang :" << namaBarang <<endl;
+        cout << "kode barang :" << kodeBarang <<endl;
+    }
+
+
+};
+
+int main()
+{
+    barang a (1122, "Buku");
+    a.output();
+    barang b (1133, "Spidol");
+    b.output ();
+    barang c(1144, "Kertas");
+    c.output();
+
+}
